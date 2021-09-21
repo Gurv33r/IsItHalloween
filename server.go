5ca3846a2b553e04845503f49b778d3607b6efb8
Gurv33r <gurveersingh477@gmail.com>
2021-09-21 11:45:34 -0700

    key board handler

--- server.go
+++ server.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -15,12 +10,12 @@ import (
 )
 
 func main() {
+	server.KeyboardInterruptHandler()
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Println(".env file does not exist")
+		log.Fatal(".env file does not exist")
 	}
 	PORT := os.Getenv("PORT")
-	fmt.Println(PORT)
 
 	router := gin.Default()
 
@@ -32,23 +27,5 @@ func main() {
 }
 
 func receive(router *gin.Engine) {
-	router.POST("/check", halloweenCheck)
-}
-
-func halloweenCheck(ctx *gin.Context) {
-	reqbody, err := ioutil.ReadAll(ctx.Request.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	reqdate := &server.Date{Str: ""}
-	if err := json.Unmarshal(reqbody, &reqdate); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Data =", reqdate.Str)
-	monthAndDay := strings.Split(reqdate.Str, "-")[1:]
-	res := "No"
-	if strings.Compare(monthAndDay[0], "10") == 0 && strings.Compare(monthAndDay[1], "31") == 0 {
-		res = "Yes"
-	}
-	ctx.String(http.StatusOK, res)
+	router.POST("/check", server.HalloweenCheck)
 }
--- server/date.go
+++ server/date.go
@@ -1,5 +1,33 @@
 package server
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
 type Date struct {
 	Str string `json:"currentDate"`
+}
+
+func HalloweenCheck(ctx *gin.Context) {
+	reqbody, err := ioutil.ReadAll(ctx.Request.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	reqdate := &Date{Str: ""}
+	if err := json.Unmarshal(reqbody, &reqdate); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Data =", reqdate.Str)
+	monthAndDay := strings.Split(reqdate.Str, "-")[1:]
+	res := "No"
+	if strings.Compare(monthAndDay[0], "10") == 0 && strings.Compare(monthAndDay[1], "31") == 0 {
+		res = "Yes"
+	}
+	ctx.String(http.StatusOK, res)
 }
